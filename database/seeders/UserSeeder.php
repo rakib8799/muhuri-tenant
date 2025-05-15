02baa49257d628e75cd49dfd778d3f29b46974b4
rakib8799 <mkrakib007@gmail.com>
2025-05-15 11:09:51 +0600

    Update UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -14,10 +15,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
+        User::create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('12345')
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345'),
+            'remember_token' => Str::random(10)
         ]);
     }
 }
